e548829c91bd12bb7c40f5b6bc9295c670e06595
Alexandra Lengen <Alexandra Lengen@AlexandraLengen>
2016-05-09 13:54:53 +0200

    extern

--- Client_Extern/src/extern/Login.java
+++ Client_Extern/src/extern/Login.java
@@ -1,35 +1,23 @@
 package extern;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-public class Login extends Application  {
+public class Login extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			primaryStage.setScene(new Scene(FXMLLoader.load(getClass()
-					.getResource("extern/Login.fxml"))));
+			stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(
+					"extern/Login.fxml"))));
 		} catch (IOException e) {
 			// MainView.showError(e);
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		}
-		primaryStage.show();
+		stage.show();
 	}
-
-	
 
 	public static void main(String[] args) {
 		launch(args);
