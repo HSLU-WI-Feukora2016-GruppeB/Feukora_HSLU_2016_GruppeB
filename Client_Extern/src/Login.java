6d35eeafbcdf6a332d8ba73eb8f44e42ba4ca001
Alexandra Lengen <Alexandra Lengen@AlexandraLengen>
2016-05-09 11:10:49 +0200

    new

---
+++ Client_Extern/src/Login.java
@@ -0,0 +1,11 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Login extends Application{
+	
+	@FXML
+	private TextField txtusername;
+
+}
