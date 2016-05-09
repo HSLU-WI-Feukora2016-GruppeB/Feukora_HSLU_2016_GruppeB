be2da9cd78a9f2b434e8bb060b81cbdc76ef93ab
Olivia <Olivia@OliviasNetbook>
2016-05-09 16:52:19 +0200

    interface auftragmanager ready

--- Businesslogik_Feukora/src/managerInterfaces/AuftragManager.java
+++ Businesslogik_Feukora/src/managerInterfaces/AuftragManager.java
@@ -1,5 +1,81 @@
 package managerInterfaces;
 
+/**
+ * Verwaltet einen Auftrag und alle Funktionen.
+ * @author Olivia
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public interface AuftragManager {
+	
+	/**
+	 * Speichert einen Auftrag.
+	 * @param entität
+	 * @return
+	 * @throws Exception
+	 */
+	public Auftrag add(Auftrag entität) throws Exception;
+	
+	/**
+	 * Passt den übergebenen Auftrag an.
+	 * @param entität
+	 * @return
+	 * @throws Exception
+	 */
+	public Auftrag update(Auftrag entität) throws Exception;
+	
+	/**
+	 * Löscht den übergebenen Auftrag.
+	 * @param entität
+	 * @return
+	 * @throws Exception
+	 */
+	public void delete(Auftrag entität) throws Exception;
 
+	/**
+	 * Liefert den Auftrag der mit der Id angefragt wurde. 
+	 * @param id
+	 * @return
+	 */
+	public Auftrag findById(Integer id);
+	
+	/**
+	 * Liefert alle Aufträge zurück.
+	 * @return
+	 */
+	public List<Auftrag> findAll();
+	
+	/**
+	 * Liefert eine Auftragliste anhand des gesuchten Terminsdatums.
+	 * @param name
+	 * @return
+	 */
+	public List<Auftrag> findByTermin(GregorianCalender termin);
+	
+	/**
+	 * Liefert eine Auftragsliste anhand des gesuchten Kontrolleur-Namens.
+	 * @param kontrolleurName
+	 * @return
+	 */
+	public List<Auftrag> findByKontrolleurName(String kontrolleurName);
+	
+	/**
+	 * Liefert eine Auftragsliste anhand des gesuchten Kontaktnamens.
+	 * @param kontaktName
+	 * @return
+	 */
+	public List<Auftrag> findByKontaktName(String kontaktName);
+	
+	/**
+	 * Liefert eine Auftragsliste anhand des gesuchten Liegenschafts-Ortes.
+	 * @param liegenschaftOrt
+	 * @return
+	 */
+	public List<Auftrag> findByLiegenschaftOrt(String liegenschaftOrt);
+	
+	
+	
+
+	
 }
