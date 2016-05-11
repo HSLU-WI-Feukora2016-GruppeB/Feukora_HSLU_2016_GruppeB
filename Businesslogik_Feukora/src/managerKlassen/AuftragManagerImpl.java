055c1d639c9959af6fc28d924d34a203a07f034f
Berri <Berri@10.3.96.55>
2016-05-11 11:38:08 +0200

    ..

--- Businesslogik_Feukora/src/managerKlassen/AuftragManagerImpl.java
+++ Businesslogik_Feukora/src/managerKlassen/AuftragManagerImpl.java
@@ -2,6 +2,10 @@ package managerKlassen;
 
 import java.util.List;
 
+import managerInterfaces.Auftrag;
+import managerInterfaces.AuftragManager;
+import managerInterfaces.GregorianCalender;
+
 
 /**
  * Stellt die Implementierung von Methoden der Schnittstelle AuftragManager zur Verfügung.
@@ -41,6 +45,66 @@ public class AuftragManagerImpl implements AuftragManager {
 	public void delete(Auftrag entity) throws Exception {
 		AuftragDAO.deleteAuftrag(entity);
 
+	}
+
+	@Override
+	public Auftrag add(Auftrag entity) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Auftrag update(Auftrag entity) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Auftrag findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByDatum(GregorianCalender datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByMitarbeiter(String mitarbeiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByKontakt(String kontakt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByLiegenschaft(String liegenschaft) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByAuftragsNummer(String auftragsNummer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Auftrag> findByMessung(String messung) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
