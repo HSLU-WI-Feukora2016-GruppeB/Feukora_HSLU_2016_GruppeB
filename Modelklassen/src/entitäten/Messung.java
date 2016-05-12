a7334767437ea215656da9ca742623230d80c615
Berri <Berri@10.3.96.21>
2016-05-12 14:07:38 +0200

    messung modelklasse überarbeitet

--- Modelklassen/src/entitäten/Messung.java
+++ Modelklassen/src/entitäten/Messung.java
@@ -11,16 +11,27 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+// enthält GregorienKalender...
+
 /**
  * Messung enthält alle Messergebnisse die der Feuerungskontrolleur beim Wärmeerzeuger abliest.
- * @author Olivia
+ * @author Matthias
  * @version 1.0
  * @since 1.0
  *
  */
 @Entity
 @NamedQueries({
-	@NamedQuery(name = "Messung.findByMessDatum", query = "SELECT m FROM Messung m WHERE m.messDatum=:messDatum")
+	@NamedQuery(name = "Messung.findByMessDatum", query = "SELECT m FROM Messung m WHERE m.messDatum=:messDatum"),
+	@NamedQuery(name = "Messung.findByRusszahl", query = "SELECT m FROM Messung m WHERE m.russzahl=:russzahl"),
+	@NamedQuery(name = "Messung.findByKohlenstoff", query = "SELECT m FROM Messung m WHERE m.kohlenstoff=:kohlenstoff"),
+	@NamedQuery(name = "Messung.findByOelanteil", query = "SELECT m FROM Messung m WHERE m.oelanteil=:oelanteil"),
+	@NamedQuery(name = "Messung.findByStickstoffgehalt", query = "SELECT m FROM Messung m WHERE m.stickstoffgehalt=:stickstoffgehalt"),
+	@NamedQuery(name = "Messung.findByAbgastemperatur", query = "SELECT m FROM Messung m WHERE m.abgastemperatur=:abgastemperatur"),
+	@NamedQuery(name = "Messung.findByWaermeerzeugertemperatur", query = "SELECT m FROM Messung m WHERE m.waermeerzeugertemperatur=:waermeerzeugertemperatur"),
+	@NamedQuery(name = "Messung.findByVerbrennungstemperatur", query = "SELECT m FROM Messung m WHERE m.verbrennungstemperatur=:verbrennungstemperatur"),
+	@NamedQuery(name = "Messung.findByO2gehalt", query = "SELECT m FROM Messung m WHERE m.o2gehalt=:o2gehalt"),
+	@NamedQuery(name = "Messung.findByAbgasverluste", query = "SELECT m FROM Messung m WHERE m.abgasverluste=:abgasverluste")
 })
 public class Messung implements Serializable{
 	
@@ -37,13 +48,13 @@ public class Messung implements Serializable{
 	
 	private int kohlenstoff;
 	
-	private int ölanteil;
+	private int oelanteil;
 	
 	private int stickstoffgehalt;
 	
 	private int abgastemperatur;
 	
-	private int wärmeerzeugertemperatur;
+	private int waermeerzeugertemperatur;
 	
 	private int verbrennungstemperatur;
 	
@@ -89,12 +100,12 @@ public class Messung implements Serializable{
 		this.kohlenstoff = kohlenstoff;
 	}
 
-	public int getÖlanteil() {
-		return ölanteil;
+	public int getOelanteil() {
+		return oelanteil;
 	}
 
-	public void setÖlanteil(int ölanteil) {
-		this.ölanteil = ölanteil;
+	public void setOelanteil(int oelanteil) {
+		this.oelanteil = oelanteil;
 	}
 
 	public int getStickstoffgehalt() {
@@ -113,12 +124,12 @@ public class Messung implements Serializable{
 		this.abgastemperatur = abgastemperatur;
 	}
 
-	public int getWärmeerzeugertemperatur() {
-		return wärmeerzeugertemperatur;
+	public int getWaermeerzeugertemperatur() {
+		return waermeerzeugertemperatur;
 	}
 
-	public void setWärmeerzeugertemperatur(int wärmeerzeugertemperatur) {
-		this.wärmeerzeugertemperatur = wärmeerzeugertemperatur;
+	public void setWaermeerzeugertemperatur(int waermeerzeugertemperatur) {
+		this.waermeerzeugertemperatur = waermeerzeugertemperatur;
 	}
 
 	public int getVerbrennungstemperatur() {
@@ -145,7 +156,21 @@ public class Messung implements Serializable{
 		this.abgasverluste = abgasverluste;
 	}
 	
-	
+	@Override
+	public String toString(){
+		return "Messung:" + "\n"
+				+ "Messdatum: \t \t \t" + messDatum + "\n"
+				+ "Russzahl: \t \t \t" + russzahl + "\n"
+				+ "Kohlenstoff: \t \t" + kohlenstoff + "\n"
+				+ "Oelanteil: \t \t \t \t" + oelanteil + "\n"
+				+ "Stickstoffgehalt: \t \t t" + stickstoffgehalt + "\n"
+				+ "Abgastemperatur: \t \t \t" + abgastemperatur + "\n"
+				+ "Waermeerzeugertemperatur: \t \t \t \t" + waermeerzeugertemperatur + "\n"
+				+ "Verbrennungstemperatur: \t \t \t \t" + verbrennungstemperatur + "\n"
+				+ "O2Gehalt: \t \t \t \t" + o2gehalt + "\n"
+				+ "Abgasverluste: \t \t \t \t" + abgasverluste + "\n";
+				
+	}
 	
 	
 
