fb723de0f13435d94995c46cbd39e2402b65c810
Olivia <Olivia@10.4.97.8>
2016-05-13 10:35:56 +0200

    modelklassen ready bis auf gregoriancal format für toSTring

--- Modelklassen/src/entitäten/Messung.java
+++ Modelklassen/src/entitäten/Messung.java
@@ -11,16 +11,21 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+// enthält GregorienKalender...
+
 /**
  * Messung enthält alle Messergebnisse die der Feuerungskontrolleur beim Wärmeerzeuger abliest.
- * @author Olivia
+ * @author Matthias
  * @version 1.0
  * @since 1.0
  *
  */
 @Entity
 @NamedQueries({
-	@NamedQuery(name = "Messung.findByMessDatum", query = "SELECT m FROM Messung m WHERE m.messDatum=:messDatum")
+	@NamedQuery(name = "Messung.findById", query = "SELECT m FROM Messung m WHERE m.idMessung=:idMessung"),
+	@NamedQuery(name = "Messung.findByMessDatum", query = "SELECT m FROM Messung m WHERE m.messDatum=:messDatum"),
+	@NamedQuery(name = "Messung.findByBeurteilungOk", query = "SELECT m FROM Messung m WHERE m.beurteilungOk=:beurteilungOk"),
+	@NamedQuery(name = "Messung.findByBeurteilungNotOk", query = "SELECT m FROM Messung m WHERE m.beurteilungNotOk=:beurteilungNotOk")
 })
 public class Messung implements Serializable{
 	
@@ -37,19 +42,42 @@ public class Messung implements Serializable{
 	
 	private int kohlenstoff;
 	
-	private int ölanteil;
+	private int oelanteil;
 	
 	private int stickstoffgehalt;
 	
 	private int abgastemperatur;
 	
-	private int wärmeerzeugertemperatur;
+	private int waermeerzeugertemperatur;
 	
 	private int verbrennungstemperatur;
 	
 	private int o2gehalt;
 	
 	private int abgasverluste;
+	
+	private boolean beurteilungOk;
+	
+	
+	private boolean beurteilungNotOk;
+	
+	private boolean russzahlNotOk;
+	
+	private boolean oelanteilenNotOk;
+	
+	private boolean abgasverlusteNotOk;
+	
+	private boolean coMgNotOk;
+	
+	private boolean noMgNotOk;
+	
+	private boolean einregulierungInnert30;
+	
+	private boolean einregulierungNichtMoeglich;
+	
+	
+	
+	
 	
 	//standardkonstruktor
 	public Messung(){
@@ -89,12 +117,12 @@ public class Messung implements Serializable{
 		this.kohlenstoff = kohlenstoff;
 	}
 
-	public int getÖlanteil() {
-		return ölanteil;
+	public int getOelanteil() {
+		return oelanteil;
 	}
 
-	public void setÖlanteil(int ölanteil) {
-		this.ölanteil = ölanteil;
+	public void setOelanteil(int oelanteil) {
+		this.oelanteil = oelanteil;
 	}
 
 	public int getStickstoffgehalt() {
@@ -113,12 +141,12 @@ public class Messung implements Serializable{
 		this.abgastemperatur = abgastemperatur;
 	}
 
-	public int getWärmeerzeugertemperatur() {
-		return wärmeerzeugertemperatur;
+	public int getWaermeerzeugertemperatur() {
+		return waermeerzeugertemperatur;
 	}
 
-	public void setWärmeerzeugertemperatur(int wärmeerzeugertemperatur) {
-		this.wärmeerzeugertemperatur = wärmeerzeugertemperatur;
+	public void setWaermeerzeugertemperatur(int waermeerzeugertemperatur) {
+		this.waermeerzeugertemperatur = waermeerzeugertemperatur;
 	}
 
 	public int getVerbrennungstemperatur() {
@@ -145,7 +173,104 @@ public class Messung implements Serializable{
 		this.abgasverluste = abgasverluste;
 	}
 	
-	
+	public boolean isBeurteilungOk() {
+		return beurteilungOk;
+	}
+
+	public void setBeurteilungOk(boolean beurteilungOk) {
+		this.beurteilungOk = beurteilungOk;
+	}
+
+	public boolean isBeurteilungNotOk() {
+		return beurteilungNotOk;
+	}
+
+	public void setBeurteilungNotOk(boolean beurteilungNotOk) {
+		this.beurteilungNotOk = beurteilungNotOk;
+	}
+
+	public boolean isRusszahlNotOk() {
+		return russzahlNotOk;
+	}
+
+	public void setRusszahlNotOk(boolean russzahlNotOk) {
+		this.russzahlNotOk = russzahlNotOk;
+	}
+
+	public boolean isOelanteilenNotOk() {
+		return oelanteilenNotOk;
+	}
+
+	public void setOelanteilenNotOk(boolean oelanteilenNotOk) {
+		this.oelanteilenNotOk = oelanteilenNotOk;
+	}
+
+	public boolean isAbgasverlusteNotOk() {
+		return abgasverlusteNotOk;
+	}
+
+	public void setAbgasverlusteNotOk(boolean abgasverlusteNotOk) {
+		this.abgasverlusteNotOk = abgasverlusteNotOk;
+	}
+
+	public boolean isCoMgNotOk() {
+		return coMgNotOk;
+	}
+
+	public void setCoMgNotOk(boolean coMgNotOk) {
+		this.coMgNotOk = coMgNotOk;
+	}
+
+	public boolean isNoMgNotOk() {
+		return noMgNotOk;
+	}
+
+	public void setNoMgNotOk(boolean noMgNotOk) {
+		this.noMgNotOk = noMgNotOk;
+	}
+
+	public boolean isEinregulierungInnert30() {
+		return einregulierungInnert30;
+	}
+
+	public void setEinregulierungInnert30(boolean einregulierungInnert30) {
+		this.einregulierungInnert30 = einregulierungInnert30;
+	}
+
+	public boolean isEinregulierungNichtMoeglich() {
+		return einregulierungNichtMoeglich;
+	}
+
+	public void setEinregulierungNichtMoeglich(boolean einregulierungNichtMoeglich) {
+		this.einregulierungNichtMoeglich = einregulierungNichtMoeglich;
+	}
+
+	@Override
+	public String toString(){
+		return "Messung:" + "\n"
+				+ "Messdatum: \t \t \t" + messDatum + "\n"
+				+ "Russzahl: \t \t \t" + russzahl + "\n"
+				+ "Kohlenstoff: \t \t \t" + kohlenstoff + "\n"
+				+ "Oelanteil: \t \t \t" + oelanteil + "\n"
+				+ "Stickstoffgehalt: \t \t \t" + stickstoffgehalt + "\n"
+				+ "Abgastemperatur: \t \t \t" + abgastemperatur + "\n"
+				+ "Waermeerzeugertemperatur: \t \t \t" + waermeerzeugertemperatur + "\n"
+				+ "Verbrennungstemperatur: \t \t \t" + verbrennungstemperatur + "\n"
+				+ "O2Gehalt: \t \t \t" + o2gehalt + "\n"
+				+ "Abgasverluste: \t \t \t" + abgasverluste + "\n"
+				
+				+ "Die geltenden LRV-Grenzwerte werden eingehalten. Es sind keine Massnahmen nötig.: \n" + beurteilungOk + "\n"
+				+ "Die Anlage wird beanstandet wegen Überschreibung von: \n" + beurteilungNotOk + "\n"
+				+ "Russzahl \t" + russzahlNotOk + "\n"
+				+ "Ölanteilen: \t" + oelanteilenNotOk + "\n"
+				+ "CO in mg/m3: \t" + coMgNotOk + "\n"
+				+ "NO2 in mg/m3: \t" + noMgNotOk + "\n"
+				+ "Abgasverluste: \t" + abgasverlusteNotOk + "\n"
+				+ "Die Anlage muss innert 30 Tagen einreguliert werden. Die erfolgte Einregulierung ist durch den Brennermonteur mit der beiligenden Rückmeldekarte zu bestätigen: \n" + einregulierungInnert30 + "\n"
+				+ "Einregulierung auf Einhaltung der LRV-Grenzwerte ist nicht möglich: \n" + einregulierungNichtMoeglich + "\n";
+				
+				
+	}
 	
 	
 
--- Modelklassen/src/entitäten/Waermeerzeuger.java
+++ Modelklassen/src/entitäten/Waermeerzeuger.java
@@ -1,20 +1,16 @@
 package entitäten;
 
 import java.io.Serializable;
-import java.util.GregorianCalendar;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-// enthält GregorienKalender...
 
 /**
- * Messung enthält alle Messergebnisse die der Feuerungskontrolleur beim Wärmeerzeuger abliest.
+ * Waermeerzeuger erzeugt Waerme für Feuerungsanlage, zusammen mit Brenner
  * @author Matthias
  * @version 1.0
  * @since 1.0
@@ -22,155 +18,72 @@ import javax.persistence.TemporalType;
  */
 @Entity
 @NamedQueries({
-	@NamedQuery(name = "Messung.findByMessDatum", query = "SELECT m FROM Messung m WHERE m.messDatum=:messDatum"),
-	@NamedQuery(name = "Messung.findByRusszahl", query = "SELECT m FROM Messung m WHERE m.russzahl=:russzahl"),
-	@NamedQuery(name = "Messung.findByKohlenstoff", query = "SELECT m FROM Messung m WHERE m.kohlenstoff=:kohlenstoff"),
-	@NamedQuery(name = "Messung.findByOelanteil", query = "SELECT m FROM Messung m WHERE m.oelanteil=:oelanteil"),
-	@NamedQuery(name = "Messung.findByStickstoffgehalt", query = "SELECT m FROM Messung m WHERE m.stickstoffgehalt=:stickstoffgehalt"),
-	@NamedQuery(name = "Messung.findByAbgastemperatur", query = "SELECT m FROM Messung m WHERE m.abgastemperatur=:abgastemperatur"),
-	@NamedQuery(name = "Messung.findByWaermeerzeugertemperatur", query = "SELECT m FROM Messung m WHERE m.waermeerzeugertemperatur=:waermeerzeugertemperatur"),
-	@NamedQuery(name = "Messung.findByVerbrennungstemperatur", query = "SELECT m FROM Messung m WHERE m.verbrennungstemperatur=:verbrennungstemperatur"),
-	@NamedQuery(name = "Messung.findByO2gehalt", query = "SELECT m FROM Messung m WHERE m.o2gehalt=:o2gehalt"),
-	@NamedQuery(name = "Messung.findByAbgasverluste", query = "SELECT m FROM Messung m WHERE m.abgasverluste=:abgasverluste")
+	@NamedQuery(name = "Waermeerzeuger.findById", query = "SELECT w FROM Waermeerzeuger w WHERE w.idWaermeerzeuger=:idWaermeerzeuger"),
+	@NamedQuery(name = "Waermeerzeuger.findByTyp", query = "SELECT w FROM Waermeerzeuger w WHERE w.waermeerzeugerTyp=:waermeerzeugerTyp"),
+	@NamedQuery(name = "Waermeerzeuger.findByBrennstoff", query = "SELECT w FROM Waermeerzeuger w WHERE w.brennstoff=:brennstoff")
 })
 public class Waermeerzeuger implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+
+	private static final long serialVersionUID = 5006295626619515437L;
 	
 	@Id
 	@GeneratedValue
-	private int idMessung;
+	private Integer idWaermeerzeuger;
 	
-	@Temporal(TemporalType.DATE)
-	private GregorianCalendar messDatum;
-	
-	private int russzahl;
-	
-	private int kohlenstoff;
-	
-	private int oelanteil;
-	
-	private int stickstoffgehalt;
-	
-	private int abgastemperatur;
-	
-	private int waermeerzeugertemperatur;
-	
-	private int verbrennungstemperatur;
-	
-	private int o2gehalt;
-	
-	private int abgasverluste;
+	private int brennstoff;
+	private String waermeerzeugerTyp;
+
 	
 	//standardkonstruktor
 	public Waermeerzeuger(){
 		
 	}
-	
+
 	//getter&setter
-	public int getIdMessung() {
-		return idMessung;
+	public Integer getIdWaermeerzeuger() {
+		return idWaermeerzeuger;
 	}
 
-	public void setIdMessung(int idMessung) {
-		this.idMessung = idMessung;
+	public void setIdWaermeerzeuger(int idWaermeerzeuger) {
+		this.idWaermeerzeuger = idWaermeerzeuger;
 	}
 
-	public GregorianCalendar getMessDatum() {
-		return messDatum;
+	public String getBrennstoff() {
+		
+		String brennS = null;
+		
+		switch (this.brennstoff){
+			case 1: brennS = "Öl";
+				break;
+			case 2: brennS = "Erdgas";
+				break;
+			case 3: brennS = "Flüssiggas";
+				break;
+		}
+		
+		return brennS;
+	}
+		
+	public void setBrennstoff(int brennstoff) {
+		this.brennstoff = brennstoff;
 	}
 
-	public void setMessDatum(GregorianCalendar messDatum) {
-		this.messDatum = messDatum;
+	public String getWaermeerzeugerTyp() {
+		return waermeerzeugerTyp;
 	}
 
-	public int getRusszahl() {
-		return russzahl;
-	}
-
-	public void setRusszahl(int russzahl) {
-		this.russzahl = russzahl;
-	}
-
-	public int getKohlenstoff() {
-		return kohlenstoff;
-	}
-
-	public void setKohlenstoff(int kohlenstoff) {
-		this.kohlenstoff = kohlenstoff;
-	}
-
-	public int getOelanteil() {
-		return oelanteil;
-	}
-
-	public void setOelanteil(int oelanteil) {
-		this.oelanteil = oelanteil;
-	}
-
-	public int getStickstoffgehalt() {
-		return stickstoffgehalt;
-	}
-
-	public void setStickstoffgehalt(int stickstoffgehalt) {
-		this.stickstoffgehalt = stickstoffgehalt;
-	}
-
-	public int getAbgastemperatur() {
-		return abgastemperatur;
-	}
-
-	public void setAbgastemperatur(int abgastemperatur) {
-		this.abgastemperatur = abgastemperatur;
-	}
-
-	public int getWaermeerzeugertemperatur() {
-		return waermeerzeugertemperatur;
-	}
-
-	public void setWaermeerzeugertemperatur(int waermeerzeugertemperatur) {
-		this.waermeerzeugertemperatur = waermeerzeugertemperatur;
-	}
-
-	public int getVerbrennungstemperatur() {
-		return verbrennungstemperatur;
-	}
-
-	public void setVerbrennungstemperatur(int verbrennungstemperatur) {
-		this.verbrennungstemperatur = verbrennungstemperatur;
-	}
-
-	public int getO2gehalt() {
-		return o2gehalt;
-	}
-
-	public void setO2gehalt(int o2gehalt) {
-		this.o2gehalt = o2gehalt;
-	}
-
-	public int getAbgasverluste() {
-		return abgasverluste;
-	}
-
-	public void setAbgasverluste(int abgasverluste) {
-		this.abgasverluste = abgasverluste;
+	public void setWaermeerzeugerTyp(String waermeerzeugerTyp) {
+		this.waermeerzeugerTyp = waermeerzeugerTyp;
 	}
 	
 	@Override
-	public String toString(){
-		return "Messung:" + "\n"
-				+ "Messdatum: \t \t \t" + messDatum + "\n"
-				+ "Russzahl: \t \t \t" + russzahl + "\n"
-				+ "Kohlenstoff: \t \t \t" + kohlenstoff + "\n"
-				+ "Oelanteil: \t \t \t" + oelanteil + "\n"
-				+ "Stickstoffgehalt: \t \t \t" + stickstoffgehalt + "\n"
-				+ "Abgastemperatur: \t \t \t" + abgastemperatur + "\n"
-				+ "Waermeerzeugertemperatur: \t \t \t" + waermeerzeugertemperatur + "\n"
-				+ "Verbrennungstemperatur: \t \t \t" + verbrennungstemperatur + "\n"
-				+ "O2Gehalt: \t \t \t" + o2gehalt + "\n"
-				+ "Abgasverluste: \t \t \t" + abgasverluste + "\n";
-				
+	public String toString() {
+		return "Waermeerzeuger: " + "\n"
+				+ "Brennstoff: \t \t \t" + brennstoff + "(1=Öl/2=Erdgas/3=Flüssiggas)" + "\n"
+				+ "Waermeerzeugertyp: \t \t" + waermeerzeugerTyp
+				;
 	}
 	
 	
+
 }
