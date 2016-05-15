464c6597e9cbbe9a93ea428944a6f21fff34541d
Berri <Berri@Lenovo-PC.home>
2016-05-16 01:31:48 +0200

    class/interface create, AuftragRo/Impl begonnen

--- rmi/src/rmi/AuftragRO.java
+++ rmi/src/rmi/AuftragRO.java
@@ -1,5 +1,45 @@
 package rmi;
 
-public interface AuftragRO {
+import java.util.List;
 
+import managerKlassen.Auftrag;
+
+/**
+ * Das Interface stellt die CRUD-Funktionen für den Auftrag zur Verfügung.
+ * 
+ * @author Berri
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface AuftragRO {
+	
+	/** 
+	 * Speichert die Aenderung.
+	 * @param entity
+	 * @throws Exception
+	 */
+	public Auftrag add(Auftrag entity) throws Exception;
+	
+	/**
+	 * Updatet die Aenderung.
+	 * @param entity
+	 * @throws Exception
+	 */
+	public Auftrag update(Auftrag entity) throws Exception;
+	
+	/**
+	 * Löscht die Aenderung.
+	 * @param entity
+	 * @throws Exception
+	 */
+	public Auftrag delete(Auftrag entity) throws Exception;
+	
+	/**
+	 * Fuegt alle Aenderungen in eine Liste ein.
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Auftrag> findAll() throws Exception;
+	
 }
--- rmi/src/rmi/AuftragROImpl.java
+++ rmi/src/rmi/AuftragROImpl.java
@@ -1,5 +1,35 @@
 package rmi;
 
-public class AuftragROImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 
-}
+public class AuftragROImpl extends UnicastRemoteObject implements AuftragRO {
+
+	private static final long serialVersionUID = 4989622193039156812L;
+	
+	private AuftragManager auftragManager;
+	
+	public AuftragROImpl() throws RemoteException {
+		auftragManager = new AuftragManagerImpl();
+	}
+
+	@Override
+	public Auftrag add(Auftrag entity) throws Exception {
+		return auftragManager.add(entity);
+	}
+
+	@Override
+	public Auftrag update(Auftrag entity) throws Exception {
+		return auftragManager.update(entity);
+	}
+
+	@Override
+	public Auftrag delete(Auftrag entity) throws Exception {
+		return auftragManager.delete(entity);
+	}
+
+	@Override
+	public List<Auftrag> findAll() throws Exception {
+		return auftragManager.findAll();
+	}}
