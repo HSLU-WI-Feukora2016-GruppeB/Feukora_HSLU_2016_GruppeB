d8a74d38c2cbebee4cc9b99ec86eaf121b2166d6
Olivia <Olivia@10.4.96.126>
2016-05-19 17:23:04 +0200

    new all test persister

--- Persister/test/webservice/gruppeB/persister/AllTests.java
+++ Persister/test/webservice/gruppeB/persister/AllTests.java
@@ -1,27 +1,14 @@
-/*
- * ZWECK: Praxisprojekt
- * MODUL: Softwarekomponenten, HSLU-Wirtschaft
- */
-
 package webservice.gruppeB.persister;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-/**
- * AllTests
- * 
- * @version 1.0
- * @author Luca Raneri
- *
- */
 @RunWith(Suite.class)
-@SuiteClasses({ AuftragDAOTest.class, BrennerDAOTest.class,
-		FeuerungsanlageDAOTest.class, KontaktDAOTest.class,
-		LiegenschaftDAOTest.class, MessungDAOTest.class,
-		MitarbeiterDAOTest.class, OrtDAOTest.class, BenutzerDAOTest.class,
-		WaermeerzeugerDAOTest.class })
+@SuiteClasses({ AuftragDAOTest.class, BenutzerDAOTest.class,
+		BrennerDAOTest.class, FeuerungsanlageDAOTest.class,
+		KontaktDAOTest.class, LiegenschaftDAOTest.class, MessungDAOTest.class,
+		MitarbeiterDAOTest.class, OrtDAOTest.class, WaermeerzeugerDAOTest.class })
 public class AllTests {
 
 }
