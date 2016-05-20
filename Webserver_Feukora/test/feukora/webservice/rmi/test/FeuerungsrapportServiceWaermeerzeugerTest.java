deb316a5d9068e2c60c2c189051dd92e306ced9a
Luca Raneri <Luca Raneri@10.4.97.77>
2016-05-20 20:19:21 +0200

    WebServer Tests bereit.

--- Webserver_Feukora/test/feukora/webservice/rmi/test/FeuerungsrapportServiceWaermeerzeugerTest.java
+++ Webserver_Feukora/test/feukora/webservice/rmi/test/FeuerungsrapportServiceWaermeerzeugerTest.java
@@ -2,12 +2,16 @@ package feukora.webservice.rmi.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import entitys.Waermeerzeuger;
 import feukora.webservice.rmi.FeuerungsrapportService;
 import feukora.webservice.rmi.FeuerungsrapportServiceImpl;
 
@@ -48,39 +52,153 @@ public class FeuerungsrapportServiceWaermeerzeugerTest {
 		deleteAll();
 	}
 	
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#updateWaermeerzeuger(entitys.Waermeerzeuger)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testUpdateWaermeerzeuger() {
-		fail("Not yet implemented");
+	public void testUpdateWaermeerzeuger() throws Exception{
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 3);
+		
+		Waermeerzeuger w = fservice.findWaermeerzeugerByTyp("qwertz").get(0);
+		assertNotNull(w);
+		w.setWaermeerzeugerTyp("Pagani");
+		
+		fservice.updateWaermeerzeuger(w);
+		
+		Waermeerzeuger aDB = fservice.findWaermeerzeugerByTyp("Pagani").get(0);
+		assertNotNull(aDB);
+		assertTrue(aDB.getWaermeerzeugerTyp() != "qwertz");
+		
+		waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 3);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#findAllWaermeerzeuger()}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testFindAllWaermeerzeuger() {
-		fail("Not yet implemented");
+	public void testFindAllWaermeerzeuger() throws Exception{
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 3);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#findWaermeerzeugerByTyp(String)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testFindWaermeerzeugerByTyp() {
-		fail("Not yet implemented");
+	public void testFindWaermeerzeugerByTyp() throws Exception{
+
+		String typ = "qwertz";
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findWaermeerzeugerByTyp(typ);
+		assertTrue(waermeerzeugerListe.size() == 1);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#findWaermeerzeugerByBrennstoff(int)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testFindWaermeerzeugerByBrennstoff() {
-		fail("Not yet implemented");
+	public void testFindWaermeerzeugerByBrennstoff() throws Exception {
+
+		int brennstoff = 2;
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findWaermeerzeugerByBrennstoff(brennstoff);
+		assertTrue(waermeerzeugerListe.size() == 1);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#findWaermeerzeugerByBaujahr(int)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testFindWaermeerzeugerByBaujahr() {
-		fail("Not yet implemented");
+	public void testFindWaermeerzeugerByBaujahr() throws Exception {
+
+		int baujahr = 2010;
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findWaermeerzeugerByBaujahr(baujahr);
+		assertTrue(waermeerzeugerListe.size() == 1);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#deleteWaermeerzeuger(entitys.Waermeerzeuger)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testDeleteWaermeerzeuger() {
-		fail("Not yet implemented");
+	public void testDeleteWaermeerzeuger() throws Exception {
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 3);
+
+		fservice.deleteWaermeerzeuger(waermeerzeugerListe.get(0));
+
+		waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 2);
 	}
 
+	/**
+	 * Dieser Test tested die Methode {@link FeuerungsrapportServiceImpl#deleteWaermeerzeugerById(Integer)}.
+	 * @throws Exception
+	 */
 	@Test
-	public void testDeleteWaermeerzeugerById() {
-		fail("Not yet implemented");
+	public void testDeleteWaermeerzeugerById() throws Exception {
+		
+		List<Waermeerzeuger> waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 3);
+
+		fservice.deleteWaermeerzeugerById(waermeerzeugerListe.get(0).getIdWaermeerzeuger());
+
+		waermeerzeugerListe = fservice.findAllWaermeerzeuger();
+		assertTrue(waermeerzeugerListe.size() == 2);
 	}
 
+	/**
+	 * Initialisiert die Datenbank mit Testwerten.
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<Waermeerzeuger> init() throws Exception{
+		
+		deleteAll();
+		
+		List<Waermeerzeuger> lWaermeerzeuger = new ArrayList<>();
+		
+		//3 Waermeerzeuger erstellen
+		lWaermeerzeuger.add(new Waermeerzeuger(1, "qwertz", 2012));
+		lWaermeerzeuger.add(new Waermeerzeuger(2, "uiopü", 2011));
+		lWaermeerzeuger.add(new Waermeerzeuger(3, "asdfg", 2010));
+		
+		for(Waermeerzeuger w : lWaermeerzeuger){
+			fservice.addWaermeerzeuger(w);
+		}
+				
+		return lWaermeerzeuger;
+	}
+	
+	/**
+	 * Löscht alle initialisierten Daten.
+	 * @throws Exception
+	 */
+	private static void deleteAll() throws Exception {
+		
+		deleteAllWaermeerzeuger();
+	}
+	
+	/**
+	 * Löscht alle Waermeerzeuger.
+	 * @throws Exception
+	 */
+	public static void deleteAllWaermeerzeuger() throws Exception {
+
+		for (Waermeerzeuger w : fservice.findAllWaermeerzeuger()) {
+			fservice.deleteWaermeerzeuger(w);
+		}
+	}
+	
 }
